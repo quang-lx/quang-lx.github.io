6a283bb2aadfbb4652f01a432b16ffe609890703
quang-lx <quang268200@gmail.com>
2021-04-12 14:05:06 +0700

    add

--- js/inspection_result.js
+++ js/inspection_result.js
@@ -1,6 +1,8 @@
 let square_box = $('.square-box')
-let amount_row = 6
-let amount_column = 6
+let amount_row = 5
+let amount_column = 5
+let position_check_left ="50%"
+let position_check_top ="50%"
 
 function drawSquare() {
     let html_square =" <table> <tbody>"
@@ -12,7 +14,15 @@ function drawSquare() {
         html_square += "</tr>";
     } 
     html_square +=`</table> </tbody> <div class="check-note"></div>`
-
     square_box.html(html_square)
+
+}
+function setPostionCheckNote() {
+    let check_note = $('.check-note');
+    let check_note_width = $('.check-note').width()/2+"px";
+    console.log(`calc(${position_check_left}-${check_note_width})`);
+    check_note.css("left",`calc(${position_check_left} - ${check_note_width})`);
+    check_note.css("top",`calc(${position_check_top} - ${check_note_width})`);
 }
 drawSquare()
+setPostionCheckNote()
