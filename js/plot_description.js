8913c72643b17e5d004b9bbeb765bd7523e02bba
quang-lx <quang268200@gmail.com>
2021-06-17 09:51:00 +0700

    update

--- js/plot_description.js
+++ js/plot_description.js
@@ -1,14 +1,43 @@
-let square_box = $('#square-box')
+let square_box = $('.square-box')
 let amount_row = 5
 let amount_column = 5
-
 
 function drawSquare() {
     let html_square =" <table> <tbody>"
     for (let row = 0; row < amount_row; row++) {
         html_square += "<tr>";
         for (let column = 0; column <amount_column; column++) {
-            html_square+=`<td><div class="content_square"></div></td>`        
+            let styleTd =''
+            switch (row) {
+                case 0:
+                    if (column==0) {
+                        styleTd = 'border-top:none;border-left:none'
+                    }else if (column==4) {
+                        styleTd = 'border-top:none;border-right:none'        
+                    }else{
+                        styleTd = 'border-top:none'        
+                    }      
+                    break;
+                case 4:
+                    if (column ==0) {
+                        styleTd = 'border-buttom:none;border-left:none'
+                    }else if (column==4) {
+                        styleTd = 'border-buttom:none;border-right:none'        
+                    }else{
+                        styleTd = 'border-buttom:none'        
+                    }             
+                    break;
+                default:
+                    if (column ==0) {
+                        styleTd = 'border-left:none'
+                    }else if (column==4) {
+                        styleTd = 'border-right:none'        
+                    }    
+                    break;
+            }
+            
+           
+            html_square+=`<td  style=${styleTd} ><div class="content_square"></div></td>`        
         }
         html_square += "</tr>";
     } 
@@ -16,14 +45,22 @@ function drawSquare() {
     square_box.html(html_square)
 
 }
-function setPostionCheckNote(checknote,position_check_left,position_check_top) {
+let note_list=[
+    ['50%','50%'],
+    ['20%','20%'],
+    ['50%','50%'],
+    ['80%','80%'],
+]
+function setPostionCheckNote(checknote) {
     let check_note = $(`.${checknote}`);
-    let check_note_width = check_note.width()/2+"px";
-    check_note.css("left",`calc(${position_check_left} - ${check_note_width})`);
-    check_note.css("top",`calc(${position_check_top} - ${check_note_width})`);
+    for (let index = 0; index < check_note.length; index++) {
+        const element = check_note[index];
+        let check_note_width = $(element)[0].getBoundingClientRect().width/2+"px";
+        $(element).css("left",`calc(${note_list[index][0]} - ${check_note_width})`);
+        $(element).css("bottom",`calc(${note_list[index][1]} - ${check_note_width})`);
+        
+    }
+    
 }
 drawSquare()
-setPostionCheckNote('check-note','50%','50%')
-setPostionCheckNote('check-note1','22%','82%')
-setPostionCheckNote('check-note2','50%','50%')
-setPostionCheckNote('check-note3','84%','17%')
+setPostionCheckNote('check-note')
